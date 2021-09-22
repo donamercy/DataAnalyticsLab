10277453d4b7a3e16062b2481b002d3e262646c2
donamercy <88762635+donamercy@users.noreply.github.com>
2021-09-22 21:12:22 +0530

    Update assignment4_1.r

--- lab4/assignment4_1.r
+++ lab4/assignment4_1.r
@@ -1,19 +1,21 @@
 barplot(table(sample(1:5, size = 15, replace = T,prob = c(0.3,0.8,0.6,0.4,0.2))));
 
 #4.1b
-data <-table(vec);
+vec<- sample(c("A", "B", "C"), 50, replace = TRUE)
+data<-table(vec);
+print(data)
 
 cumfreq_data <- cumsum(data);
 print (cumfreq_data);
-
 
 barplot(cumfreq_data);
 
 
 #4.1c 
-n<-0:10
-probability<-dbinom(x = n,size = 10,prob = 0.16);
-plot(x = n,y = probability,type = 'h',xlab = " ",ylab = "Probability", sub = "Number of kids with blue eyes");
+x<-0:10
+y<-dbinom(0:10,10,0.16)
+data.frame("Probability" = y, row.names = x)
+plot(0:10,dbinom(0:10,10,0.16),,type='h',xlab="",ylab="Probability",sub="Number of kids with blue eyes") 
 
 
 #4.1d 
